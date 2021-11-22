4d1c5330646de43f537d939f3a55ed0c92244b0f
AndreyPapchinsky <Ha.xJle6ywek@yandex.ru>
2021-11-23 01:59:59 +0300

    Добавьте файлы проекта.

---
+++ Algorithm/Algorithm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+public class Program
+{
+
+    private static double Method(double target)
+    {
+        double x = 1;
+        double oldx;
+        do
+        {
+            oldx = x;
+            x = (x + target / x) / 2;
+        }
+        while (oldx != x);
+        return x;
+    }
+    public static void Main()
+    {
+        double target = 2023;
+
+        double x = Method(target);
+        Console.WriteLine(x);
+        Console.WriteLine(x * x);
+    }
+}
